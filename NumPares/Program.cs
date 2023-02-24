b908af10beec35b8d1160ff1465954c86cd2506d
EliasO23 <124007307+EliasO23@users.noreply.github.com>
2023-02-23 21:09:52 -0600

    Proyecto

---
+++ NumPares/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(" ");
+Console.WriteLine("BIENVENIDO ING AL BUSCADOR DE NUMEROS PARES");
+Console.WriteLine("----- Segun el intervalo determinado -----");
+Console.WriteLine(" ");
+
+Console.WriteLine("Escribe el valor inicial del intervalo:");
+int ValorI = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Escribe el valor final del intervalo:");
+int ValorF = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(" ");
+Console.WriteLine($"Los numeros pares del intervalo de {ValorI} a {ValorF} son:");
+for (int i = ValorI; i<=ValorF; i++){
+    if (i % 2 == 0){
+         Console.WriteLine(i);
+    }
+}
+Console.WriteLine(" ");
+
+    
+    
+
+
